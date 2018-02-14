eb8c987ee5599fd1e5a8fd7dd6133c764eba1588
Juha Lehtonen <juha.t.lehtonen.91@gmail.com>
2018-02-14 19:56:02 +0200

    asd

--- build.sh
+++ build.sh
@@ -7,18 +7,8 @@
 # Remove old releases
 rm -rf _build/prod/rel/*
 
-echo $DOCKNIX_DB_USER;
-echo $DOCKNIX_SECRET_KEY_BASE;
-
 # Build the image
-docker build --rm -t docknix-build -f Dockerfile.build . -e DOCKNIX_RELEASE_COOKIE='$DOCKNIX_RELEASE_COOKIE' \
--e DOCKNIX_SECRET_KEY_BASE='$DOCKNIX_SECRET_KEY_BASE' \
--e DOCKNIX_DB_USER='$DOCKNIX_DB_USER' \
--e DOCKNIX_DB_PASS='$DOCKNIX_DB_PASS'
+docker build --rm -t docknix-build -f Dockerfile.build . -e DOCKNIX_RELEASE_COOKIE=$DOCKNIX_RELEASE_COOKIE -e DOCKNIX_SECRET_KEY_BASE=$DOCKNIX_SECRET_KEY_BASE -e DOCKNIX_DB_USER=$DOCKNIX_DB_USER -e DOCKNIX_DB_PASS=$DOCKNIX_DB_PASS
 
 # Run the container
-docker run -e DOCKNIX_RELEASE_COOKIE=$DOCKNIX_RELEASE_COOKIE \
--e DOCKNIX_SECRET_KEY_BASE=$DOCKNIX_SECRET_KEY_BASE \
--e DOCKNIX_DB_USER=$DOCKNIX_DB_USER \
--e DOCKNIX_DB_PASS=$DOCKNIX_DB_PASS \
--it --rm --name docknix-build -v $(pwd)/_build/prod/rel:/opt/app/_build/prod/rel docknix-build
+docker run -e DOCKNIX_RELEASE_COOKIE=$DOCKNIX_RELEASE_COOKIE -e DOCKNIX_SECRET_KEY_BASE=$DOCKNIX_SECRET_KEY_BASE -e DOCKNIX_DB_USER=$DOCKNIX_DB_USER -e DOCKNIX_DB_PASS=$DOCKNIX_DB_PASS -it --rm --name docknix-build -v $(pwd)/_build/prod/rel:/opt/app/_build/prod/rel docknix-build
