f2dbf75d4d4ee0f8456c90d0cf02bf838e20ec93
Juha Lehtonen <juha.t.lehtonen.91@gmail.com>
2018-02-14 17:04:29 +0200

    Further configuration on CircleCI

--- .circleci/post-deploy.sh
+++ .circleci/post-deploy.sh
@@ -3,14 +3,17 @@
 ssh $APP_USER@$APP_HOST <<ENDSSH
   echo "changing dir to ./apps/$APP_NAME"
   cd ./apps/$APP_NAME;
-  echo "Stopping $APP_NAME"
-  ./bin/$APP_NAME stop;
+  echo "Cycling power on $APP_NAME"
+  docker-compose down;
+  docker-compose up;
+  echo "Cycled power on $APP_NAME"
 ENDSSH
 
 ssh $APP_USER@$APP_HOST <<ENDSSH
-  echo "changing dir to ./apps/$APP_NAME"
-  cd ./apps/$APP_NAME;
-  echo "Starting $APP_NAME"
-  PORT=4000 ./bin/$APP_NAME start;
-  echo "Finished"
+  echo "Changing dir to ./nginx"
+  cd ./nginx;
+  echo "Cycling power on the Nginx container"
+  docker-compose down;
+  docker-compose up;
+  echo "Cycled power on Nginx"
 ENDSSH
